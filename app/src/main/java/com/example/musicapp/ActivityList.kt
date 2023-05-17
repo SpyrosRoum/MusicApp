602a4dee159e48d97604a7911850754187bc3afa
Eva <104171510+gaattess@users.noreply.github.com>
2023-05-17 20:49:50 +0300

    permission errors

--- app/src/main/java/com/example/musicapp/ActivityList.kt
+++ app/src/main/java/com/example/musicapp/ActivityList.kt
@@ -1,4 +1,6 @@
 package com.example.musicapp
+
+import android.content.pm.PackageManager
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
@@ -6,16 +8,21 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.provider.MediaStore
 import android.net.Uri
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class ActivityList : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var songsList: ArrayList<Song>
+    private val PERMISSIONS_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        checkPermissions()
 
         songsList = getSongs()
 
@@ -30,41 +37,78 @@ class ActivityList : AppCompatActivity() {
         }
     }
 
-    private fun getSongs() : ArrayList<Song> {
+    private fun getSongs(): ArrayList<Song> {
         val songs = ArrayList<Song>()
 
         val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         val wantedColumns = arrayOf(
-                MediaStore.Audio.Media.DISPLAY_NAME,
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.DATA, // For Image
-                MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.DATA, // For Image
+            MediaStore.Audio.Media.ARTIST,
         )
 
         val songsCursor = contentResolver.query(
-                uri,
-                wantedColumns,
-                null,
-                null,
-                MediaStore.Audio.Media.DISPLAY_NAME
+            uri,
+            wantedColumns,
+            null,
+            null,
+            MediaStore.Audio.Media.DISPLAY_NAME
         ) ?: return songs
 
         songsCursor.moveToFirst()
         do {
-            val songName = songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+            val songName =
+                songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
             val artistName =
-                    songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
-            val albumName = songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
+                songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+            val albumName =
+                songsCursor.getString(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
             val imageUrl =
-                     Uri.parse(songsCursor.getString(songsCursor.getColumnIndexOrThrow((MediaStore.Audio.Media.DATA))))
+                Uri.parse(songsCursor.getString(songsCursor.getColumnIndexOrThrow((MediaStore.Audio.Media.DATA))))
             val duration =
-                    songsCursor.getDouble(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+                songsCursor.getDouble(songsCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
 
             songs.add(Song(songName, imageUrl, artistName, albumName, duration))
         } while (songsCursor.moveToNext())
 
         songsCursor.close()
         return songs
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) // TODO not finding permissions
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSIONS_REQUEST_CODE
+            )
+        } else {
+            // Permission has already been granted
+            getSongs()
+        }
+    }
+
+    @Override
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted
+                    getSongs()
+                } else {
+                    // Permission denied
+                    Toast.makeText(this, "Permission denied to read your External storage", Toast)
+                }
+            }
+        }
     }
 }
