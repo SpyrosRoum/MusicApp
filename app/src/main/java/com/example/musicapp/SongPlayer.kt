8b34dfdd65a3a8e32be31009dffbb62e6f369a57
Spyros Roum <spyros.roum@posteo.net>
2023-06-01 21:03:20 +0300

    Refactor songPlayer screen

--- app/src/main/java/com/example/musicapp/SongPlayer.kt
+++ app/src/main/java/com/example/musicapp/SongPlayer.kt
@@ -1,5 +1,6 @@
 package com.example.musicapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
@@ -28,17 +29,10 @@ class SongPlayer : AppCompatActivity() {
     private lateinit var songsList: ArrayList<Song>
     private lateinit var currentSong: Song
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.music_player)
-
-        val songIndex = intent.getIntExtra("songIndex", -1)
-        if (songIndex == -1)
-            Log.e(TAG, "Missing song index from intent")
-
-        songsList = getSongs(contentResolver)
-        currentSong = songsList[songIndex]
-
+    /**
+     * Bind the widgets of the screen to local variables
+     */
+    private fun bindWidgets() {
         // Syncing widgets with variables
         songImage = findViewById(R.id.playing_song_image)
         songTitle = findViewById(R.id.playing_song_title)
@@ -51,6 +45,25 @@ class SongPlayer : AppCompatActivity() {
         seekBar = findViewById(R.id.playing_bar)
         currentTimestamp = findViewById(R.id.current_duration)
         songDuration = findViewById(R.id.total_duration)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.music_player)
+
+        bindWidgets()
+
+        // We get to this screen from the song list, which means we should have the index
+        // of the selected song in the intents
+        val songIndex = intent.getIntExtra("songIndex", -1)
+        if (songIndex == -1) {
+            // songIndex was missing, so log it and return to the song list
+            Log.e(TAG, "Missing song index from intent")
+            startActivity(Intent(this, SongList::class.java))
+        }
+
+        songsList = getSongs(contentResolver)
+        currentSong = songsList[songIndex]
 
         Log.i(TAG, "Starting player with song #$songIndex: $currentSong")
     }
