9c652b1b846d71a4bddde7f5564e468818cd1ea6
Eva <104171510+gaattess@users.noreply.github.com>
2023-07-02 16:31:08 +0300

    FIXED PERMISSIONS!!!!!

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -3,10 +3,59 @@ package com.example.musicapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, SongPlayerClient::class.java))
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE
+            )
+        } else {
+            // You already have permission
+            startActivity(Intent(this, SongPlayerClient::class.java))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted, start the SongPlayerClient activity
+                    startActivity(Intent(this, SongPlayerClient::class.java))
+                } else {
+                    // Show a message to the user explaining why the app needs the permission
+                    Toast.makeText(
+                        this,
+                        "This app requires audio permissions to function properly.",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    // Close the app
+                    finish()
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
